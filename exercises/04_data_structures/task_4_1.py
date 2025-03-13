db4ba0bd65f82223c92333e34009a5abcaa0b4ad
Ramokx <bagrov86@gmail.com>
2025-03-13 16:16:01 +0300

    test

--- exercises/04_data_structures/task_4_1.py
+++ exercises/04_data_structures/task_4_1.py
@@ -14,5 +14,8 @@
 проверять результат.
 
 """
+from exercises.pyneng_common_functions import get_func_params_default_value
 
 nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+
+print("test git")
