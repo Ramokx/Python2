cb52f30f39ef05f3fc008850a19a75dc9deeb476
Ramokx <bagrov86@gmail.com>
2025-07-06 23:06:36 +0300

    Додедал 23 модуль

--- exercises/23_oop_special_methods/task_23_3a.py
+++ exercises/23_oop_special_methods/task_23_3a.py
@@ -41,3 +41,67 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = {}
+        self.topology = self._normalize(topology_dict)
+
+    def _normalize(self, topology_dict):
+        for key, value in topology_dict.items():
+            if key not in self.topology.keys() and key not in self.topology.values():
+                self.topology[key] = value
+        return self.topology
+
+    def delete_link(self, from_port, to_port):
+        if self.topology.get(from_port) == to_port:
+            del self.topology[from_port]
+        elif self.topology.get(to_port) == from_port:
+            del self.topology[to_port]
+        else:
+            print("Такого соединения нет")
+
+    def delete_node(self, device):
+        tmp = {}
+        for key, value in self.topology.items():
+            if device in key or device in value:
+                for key, value in self.topology.items():
+                    if key[0] != device and value[0] != device:
+                        tmp[key] = value
+                self.topology = tmp
+                break
+        else:
+            print("Такого устройства нет")
+
+    def add_link(self, from_port, to_port):
+        keys_and_values = set(self.topology) | set(self.topology.values())
+        if self.topology.get(from_port) == to_port or self.topology.get(to_port) == from_port:
+            print("Такое соединение существует")
+        elif from_port in keys_and_values or to_port in keys_and_values: # подковырка, нужно проверять еще и значениях, просто проверить, что они есть в ключах недостаточно, так как один из портов может быть просто в значениях
+            print('Соединение с одним из портов существует')
+        else:
+            self.topology[from_port] = to_port
+
+    def __add__(self, other):
+        sum_topology = self.topology.copy()
+        sum_topology.update(other.topology)
+        return Topology(sum_topology)
+
+    def __iter__(self):
+        return iter(self.topology.items())
+
+
+if __name__ == "__main__":
+    topology_example = {
+        ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
+        ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
+        ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
+        ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
+        ("R3", "Eth0/1"): ("R4", "Eth0/0"),
+        ("R3", "Eth0/2"): ("R5", "Eth0/0"),
+        ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
+        ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
+        ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
+    }
+    top = Topology(topology_example)
+    for link in top:
+        print(link)
