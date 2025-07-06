76990a39305a6f1de01ac4e59391d495de7c6d83
Ramokx <bagrov86@gmail.com>
2025-07-06 15:04:38 -0500

    2 Задание

--- exercises/23_oop_special_methods/task_23_2.py
+++ exercises/23_oop_special_methods/task_23_2.py
@@ -44,3 +44,43 @@ ValueError: Возникла ошибка
 Тест проверяет подключение с параметрами из файла devices.yaml. Там должны быть
 указаны доступные устройства.
 """
+import telnetlib
+
+
+class CiscoTelnet:
+    def __init__(self, ip, username, password, secret):
+        self.connection = telnetlib.Telnet(ip)
+        self.connection.read_until(b'Username')
+        self._write_line(username)
+        self.connection.read_until(b'Password')
+        self._write_line(password)
+        self._write_line('enable')
+        self.connection.read_until(b'Password')
+        self._write_line(secret)
+        self.connection.read_until(b'#')
+        self._write_line('terminal length 0')
+        self.connection.read_until(b"#", timeout=5)
+        
+        
+    def _write_line(self, line):
+        self.connection.write(line.encode('utf-8') + b'\n')
+        
+        
+    def send_show_command(self, show):
+        self._write_line(show)
+        output = self.connection.read_until(b'#', timeout=5).decode('utf-8').replace('\r\n', '\n')
+        return output
+    
+        
+    def __enter__(self):
+        return self
+        
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.connection.close()
+
+
+if __name__ == "__main__":
+    r1_params = {'ip': '192.168.100.1','username': 'cisco','password': 'cisco','secret': 'cisco'}
+    with CiscoTelnet(**r1_params) as r1:
+        print(r1.send_show_command('sh clock'))
